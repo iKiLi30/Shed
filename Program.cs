8259cd883da3ea69f4a799ea3fb2f79b0b096506
WIN-HIJ4FH2GJF8\user <jolahmilovski@gmail.com>
2024-09-24 14:38:23 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Shed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Shed myShed = new Shed();
+
+            Console.WriteLine(myShed.GetObjectNumber());
+            
+            Shed myShed1 = new Shed();
+
+            Console.WriteLine(myShed1.GetObjectNumber());
+            
+            Shed myShed2 = new Shed();
+
+            Console.WriteLine(myShed2.GetObjectNumber());
+
+            Shed myShed3 = new Shed();
+
+            Console.WriteLine(myShed3.GetObjectNumber());
+
+            myShed.Heigth = 10;
+            myShed.Width = 10;
+            myShed.Lengthwise = 10;
+
+            int resizeWidth = 2;
+            int resizeHeigth = 3;
+            int resizeLenthwise = 4;
+
+            int befoResize = myShed.ShedVolume();
+
+            Console.WriteLine($"{myShed.Width}\n{myShed.Heigth}\n{myShed.Lengthwise}\n{befoResize}");
+
+            myShed.ResizeShed(resizeWidth, resizeHeigth, resizeLenthwise);
+
+            int afterResize = myShed.ShedVolume();
+
+            Console.WriteLine($"After resize\n{myShed.Width}\n{myShed.Heigth}\n{myShed.Lengthwise}\n{afterResize}\t");
+
+
+
+            myShed.ResizeAndAddSize(ref resizeWidth, ref resizeHeigth, ref resizeLenthwise);
+            Console.WriteLine("Size of shed changed at : " + resizeWidth + " " + resizeHeigth + " " + resizeLenthwise);
+
+            myShed.OutResizeAndAddSize(10, out resizeWidth, out resizeHeigth, out resizeLenthwise);
+            Console.WriteLine("Size of shed OUT changed at : " + resizeWidth + " " + resizeHeigth + " " + resizeLenthwise);*/
+
+            Shed myShed = new Shed();
+            myShed.window.OpenWindow();
+
+
+        }
+    }    
+}   
---
+++ Shed.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shed
+{
+    public class Shed
+    {
+        public class Window
+        {
+            bool openWindow = false;
+            int winWidth;
+            int winHight;
+
+
+            public bool OpenWindow() ///********************************************************
+            {
+                openWindow = true;
+                return true;
+            }
+            public Window()
+            {
+                winWidth = 1;
+                winHight = 1;
+            }
+            Window window = new Window();
+            public int WinWidth
+            {
+                get { return winWidth; }
+                set { winWidth = value; }
+            }
+            public int WinHeigth
+            {
+                get { return winHight; }
+                set { winHight = value; }
+            }
+        }
+
+        static int objecNumber = 0;
+
+        public int GetObjectNumber()
+        {
+            return objecNumber;
+        }
+        public Shed()   //constructor
+        {
+            width = 1;
+            Lengthwise = 1;
+            this.height = 1;
+            objecNumber++;
+        }
+            //variable
+        int width;
+        int height;
+        string name = " ";
+
+        //property
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+        public int Heigth
+        {
+            get { return height; }
+            set { height = value; }
+        }
+
+        public int Lengthwise { get; set; }
+        public int ShedVolume()
+        {
+            return (width * height * Lengthwise);
+        }
+        public void ResizeShed(int x, int y, int z)
+        {
+            Width += x;
+            Lengthwise += z;
+            Heigth += y;
+
+        }
+        public void ResizeAndAddSize(ref int x,ref int y, ref int z)
+        {
+            ResizeShed(x,y,z);
+            Width = x;
+            Heigth = y;
+            Lengthwise =z;
+        }
+        public void OutResizeAndAddSize( int inc, out int x, out int y, out int z)
+        {
+            ResizeShed(inc, inc , inc);
+            x = Width;
+            y = Heigth;
+            z = Lengthwise;
+        }
+
+    }
+}
